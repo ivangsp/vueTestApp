524b3851e76007b32d5b1610526d47f6e8929111
ivangsp <ojiamboivan@gmail.com>
2018-12-12 01:21:43 +0200

    add function to calaculate cost of a product

--- quizz/answers.js
+++ quizz/answers.js
@@ -21,7 +21,9 @@ const { type, amount } = expense
 
 /** 
  * Qn: Write a function to check if a word/phrase/sequence is a palindrome using vanilla JS
- * Hint: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run or 4567654
+ * @summary Hint: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run or 4567654
+ * @param {string} word
+ * @returns {boolean}
  */
 
 const isPalindrome = word => {
@@ -39,7 +41,9 @@ const isPalindrome = word => {
 }
 
 /**
- * Qn3: Write a function (say fruitPresent('fruit-name') )to find out if a given input is present in the array below using
+ * @summary Qn3: Write a function (say fruitPresent('fruit-name') )to find out if a given input is present in the array below using
+ * @param {string} fruitName
+ * @returns {boolean}
  */
 const fruitPresent = fruitName => {
     let fruits = [
@@ -65,6 +69,49 @@ const fruitPresent = fruitName => {
 
 }
 
+// Qn4
+let productCart = [
+    { productname:'iphone-x',qty:10, price:1000 },
+    { productname:'macbook pro',qty:200, price:2000},
+    { productname:'iwatch',qty:100, price:550 },
+    { productname:'macbook air',qty:100, price:1000},
+    { productname:'iphone 8',qty:300, price:700 },
+    { productname:'iphone 7',qty:100, price:550 },
+    { productname:'ipad Retina',qty:20, price:1000},
+    { productname:'ipad', qty:10, price:700 },
+    { productname:'Magic Mouse',qty:50, price:300},
+    { productname:'Magic Trackpad',qty:75, price:200}
+    ]
+/**
+ *  @summary Find the total cost of all products with quantity > n using vanilla JS (ES6)
+ * @param {float} quantity
+ * @returns {float}
+ */
+   
+const CostOfProductForQtyGreaterThan = qty => {
+    return productCart
+        .filter(product => product.qty > qty)
+        .reduce((totalCost, product) => product.qty * product.price + totalCost, 0);
+}
+
+/**
+ * ii) Write a function that can take a product name and return the total cost.
+ * @summary CostOfProduct('ipad') should output 7000
+ * @param {string} productName
+ * @returns {float}
+ */
+const CostOfProduct = productName => {
+    const product =   productCart.find(product => product.productname === productName);
+    if (product) {
+       return product.qty * product.price; 
+    }
+}
+ 
+
+
 console.warn("ppp", isPalindrome(""));
 console.warn("ppp", fruitPresent("apple"));
 console.warn("ppp", fruitPresent("apple2"));
+console.warn("cost of product > 100",CostOfProductForQtyGreaterThan(300)); // 200*2000 +300*700
+console.warn("cost of product",CostOfProduct('ipad')); // 7000
+
