ba93a2e4834f0dbbfc0445ec627f1601ab3d0d3d
ivangsp <ojiamboivan@gmail.com>
2018-12-12 23:49:50 +0200

    modify fruitsPresent function

--- quizz/answers.js
+++ quizz/answers.js
@@ -47,27 +47,13 @@ const isPalindrome = word => {
  */
 const fruitPresent = fruitName => {
     let fruits = [
-        'Apple', 'Apricot', 'Avocado', 'banana', 'Blackberry', 'Blueberry', 'Boysenberry', 'Crab apples',
-        'Currant', 'Cherry', 'Cherimoya', 'Chico fruit', 'Coconut', 'Cranberry', 'Custard apple', 'Date', 'dragonfruit', 'durian',
-        'Elderberry', 'Feijoa', 'Fig', 'Goji berry', 'Gooseberry', 'grape', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry',
-        'Jabuticaba', 'Jackfruit', 'Jambul', 'jujube', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat',
-        'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon',
-        'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Blood orange', 'Clementine', 'Mandarine',
-        'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Prune', 'Pineapple', 'Pluot',
-        'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Rambutan', 'Redcurrant', 'Salal berry', 'Salak', 'Salmonberry',
-        'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Ugli fruit', 'Yuzu'
+        'Apple', 'Apricot', 'Avocado', 'banana', 'Blackberry'
     ];
 
-    const filteredFruits = fruits.filter(fruit => {
-        return fruit.toString().toLowerCase() === fruitName.toString().toLowerCase();
-    });
-    // debugger;
-    if (filteredFruits.length > 0) {
-        return true
-    } 
-    return false;
-
-}
+    return fruits
+        .map(fruit => fruit.toString().toLowerCase())
+        .includes(fruitName.toString().toLowerCase());
+};
 
 // Qn4
 let productCart = [
