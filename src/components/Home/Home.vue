fd3257a83b3663db336b63b5875a38d75fbd0202
ivangsp <ojiamboivan@gmail.com>
2018-12-14 00:04:00 +0200

    add table

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="main-container">
+    <div class="nav-container">
+      <h2>Table showing Alubums and Photos</h2>
+    </div>
+    <div class="content-container">
+      <div class="side-bar">
+        <p>Search By </p>
+        <div>
+          <select>
+          <option value="0"> All </option>
+          <option value="1"> Alubum Title </option>
+          <option value="2"> Photo title </option>
+        </select>
+        </div>
+      </div>
+      <div class="table-container">
+      <Table/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Table from '@/components/Home/Table'
+
+export default {
+  name: 'Home',
+  components: {Table}
+}
+</script>
+
+<style>
+.main-container {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  /* margin: 10px 30px 0px 30px; */
+}
+.content-container {
+  display: flex;
+  flex-direction: row;
+}
+.side-bar {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+}
+.table-container {
+  flex-grow: 3
+}
+
+select {
+  padding: 5px;
+  margin-left: 12px;
+  margin-top: 6px;
+}
+
+</style>
---
+++ src/components/Home/Table.vue
@@ -0,0 +1,85 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Album Id</th>
+        <th> Alubum Title</th>
+        <th>Photo Title</th>
+        <th>Photo Thumbnail</th>
+      </tr>
+
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>title will go here</td>
+        <td>photo title will go Here</td>
+        <td>
+          <div><img
+            src="https://via.placeholder.com/150/24f355"
+            alt=""></div>
+        </td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>title will go here</td>
+        <td>photo title will go Here</td>
+        <td>
+          <div><img
+            src="https://via.placeholder.com/150/24f355"
+            alt=""></div>
+        </td>
+      </tr>
+    </tbody>
+
+  </table>
+</template>
+<script>
+export default {
+  name: 'Table',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+table {
+  table-layout: fixed;
+  width: 100%;
+  border-collapse: collapse;
+  border: 3px solid purple;
+  padding-left: 10px;
+  /* margin: 10px; */
+}
+thead th:nth-child(1) {
+  width: 8%;
+}
+thead th:nth-child(2) {
+  width: 30%;
+}
+thead th:nth-child(3) {
+  width: 30%;
+}
+
+thead th:nth-child(4) {
+  width: 30%;
+}
+
+th, td {
+  padding: 20px;
+}
+tbody tr:nth-child(odd) {
+  background-color: #F2F2F2;
+}
+
+tbody tr:nth-child(even) {
+  background-color: #FFFFFF;
+}
+thead {
+  background-color: #3D3D3D;
+  color: white;
+}
+</style>
